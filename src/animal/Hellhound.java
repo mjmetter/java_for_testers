986473a9b0d7ebf666a27eaa0e0f19611d269b86
timlingerak <tim.lingerak@oelan.nl>
2014-09-19 17:18:46 +0200

    Hellhound toegevoegd.

--- src/animal/Hellhound.java
+++ src/animal/Hellhound.java
@@ -2,24 +2,30 @@ package animal;
 
 import general.Action;
 
+import java.util.Random;
+
+/**
+ * Created by timlingerak on 19-09-14.
+ */
+
 public class Hellhound extends Dog {
 
     @Override
     public Action getAction() {
-        if (this.getHealth() == this.getMaxHealth()){
-            return Action.SLEEP;
+        Random rand = new Random();
+        int x = rand.nextInt(4);
+        switch (x) {
+            case 0:
+                return Action.ATTACK;
+            case 1:
+                return Action.BLOCK;
+            case 2:
+                return Action.EAT;
+            case 3:
+                return Action.SLEEP;
+            default:
+                throw new IllegalArgumentException("Did you forget to put a break statement in your switch block?");
         }
-        else {
-            switch (this.getLastActionOpponent()) {
-                case BLOCK:
-                    return Action.EAT;
-                case ATTACK:
-                    return Action.BLOCK;
-                case EAT:
-                    return Action.ATTACK;
-                default:
-                    return Action.ATTACK;
-            }
-        }
+
     }
 }
